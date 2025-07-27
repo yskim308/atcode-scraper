e8ae8ac422b32ecc20f7830d08cae6c05a18ad05
Young Seo Kim <youngseokim7@gmail.com>
2025-07-27 20:04:29 +0900

    initial scrape all test

--- src/scrape-all.ts
+++ src/scrape-all.ts
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import { ScrapeService } from "./sevices/ScrapeService";
+import { delay } from "./util";}
 import type { Task } from "./types";
 
 const testLink = "https://atcoder.jp/contests/abc416/tasks";
@@ -9,10 +10,20 @@ const taskLink = "https://atcoder.jp/contests/abc416/tasks/abc416_f";
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const scrapeService = new ScrapeService(browser, page);
-  const links = await scrapeService.getTaskLinks(testLink);
-  console.log(links);
-
-  const task: Task = await scrapeService.scrapeTaskInfo(taskLink);
-  console.log(task);
+  for (let i = 46; i <= 416; ++i) {
+    const links = await scrapeService.getTaskLinks(
+      `https://atcoder.jp/contest/abc${i}/tasks`,
+    );
+    console.log("will now scrape these links:");
+    console.log(links);
+    for (const link of links) {
+      // delay between 1 and 5 seconds randomly
+      const randomDelay = Math.floor(Math.random() * 4) + 1;
+      await delay(randomDelay);
+      const task: Task = await scrapeService.scrapeTaskInfo(taskLink);
+      console.log("scraped task:");
+      console.log(task);
+    }
+  }
   await browser.close();
 })();
