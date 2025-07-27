184c75299a77154e41307fcc4ac20622eb38f650
Young Seo Kim <youngseokim7@gmail.com>
2025-07-27 18:32:31 +0900

    initial single page testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 import puppeteer from "puppeteer";
+import { ScrapeService } from "./sevices/ScrapeService";
+import type { Task } from "./types";
+
+const testLink = "https://atcoder.jp/contests/abc416/tasks";
+const taskLink = "https://atcoder.jp/contests/abc416/tasks/abc416_a";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const scrapeService = new ScrapeService(browser, page);
+  const links = await scrapeService.getTaskLinks(testLink);
+  console.log(links);
+
+  const task: Task = await scrapeService.scrapeTaskInfo(taskLink);
+  console.log(task);
+  await browser.close();
+})();
