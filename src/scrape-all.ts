026ffb12474d1da8d432a695b32678bd76b197da
Young Seo Kim <youngseokim7@gmail.com>
2025-07-27 20:12:47 +0900

    initial scrape all (no database integration yet

--- src/scrape-all.ts
+++ src/scrape-all.ts
@@ -3,25 +3,21 @@ import { ScrapeService } from "./sevices/ScrapeService";
 import { delay } from "./util";
 import type { Task } from "./types";
 
-const testLink = "https://atcoder.jp/contests/abc416/tasks";
-const taskLink = "https://atcoder.jp/contests/abc416/tasks/abc416_f";
-
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const scrapeService = new ScrapeService(browser, page);
   for (let i = 46; i <= 416; ++i) {
     let contestNumber: string = i < 100 ? `0${i}` : `${i}`;
-    const links = await scrapeService.getTaskLinks(
-      `https://atcoder.jp/contest/abc${contestNumber}/tasks`,
-    );
-    console.log("will now scrape these links:");
+    const contestLink = `https://atcoder.jp/contests/abc${contestNumber}/tasks`;
+    const links = await scrapeService.getTaskLinks(contestLink);
+    console.log(`scraping links from: ${contestLink}`);
     console.log(links);
     for (const link of links) {
       // delay between 1 and 5 seconds randomly
       const randomDelay = Math.floor(Math.random() * 4) + 1;
       await delay(randomDelay);
-      const task: Task = await scrapeService.scrapeTaskInfo(taskLink);
+      const task: Task = await scrapeService.scrapeTaskInfo(link);
       console.log("scraped task:");
       console.log(task);
     }
