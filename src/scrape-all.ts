443fbc575857b45fbe67fd4d09f5d605eea73a52
Young Seo Kim <youngseokim7@gmail.com>
2025-07-27 20:16:47 +0900

    comments for clarity

--- src/scrape-all.ts
+++ src/scrape-all.ts
@@ -7,12 +7,16 @@ import type { Task } from "./types";
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const scrapeService = new ScrapeService(browser, page);
+
   for (let i = 46; i <= 416; ++i) {
+    // getting task links
     let contestNumber: string = i < 100 ? `0${i}` : `${i}`;
     const contestLink = `https://atcoder.jp/contests/abc${contestNumber}/tasks`;
     const links = await scrapeService.getTaskLinks(contestLink);
     console.log(`scraping links from: ${contestLink}`);
     console.log(links);
+
+    // scraping each task
     for (const link of links) {
       // delay between 1 and 5 seconds randomly
       const randomDelay = Math.floor(Math.random() * 4) + 1;
