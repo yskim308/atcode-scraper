f9ff5e5a46b866c8dfc381ec262a6c1f0e89d40b
Young Seo Kim <youngseokim7@gmail.com>
2025-07-28 17:37:29 +0900

    create test file

---
+++ src/test-scrape.ts
@@ -0,0 +1,21 @@
+import { ScrapeService } from "./sevices/ScrapeService";
+import type { Task } from "./types";
+import puppeteer from "puppeteer";
+
+const testLink = "https://atcoder.jp/contests/abc416/tasks";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const scrapeService = new ScrapeService(browser, page);
+
+  const links = await scrapeService.getTaskLinks(testLink);
+  console.log(`crapingl inks from: ${testLink}`);
+  console.log(links);
+
+  for (const link of links) {
+    const task: Task = await scrapeService.scrapeTaskInfo(link);
+    console.log(`scraped task from :${link}`);
+    console.log(task);
+  }
+})();
