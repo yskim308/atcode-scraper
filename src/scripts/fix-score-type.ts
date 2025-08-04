ae2c8a02a63f3e7689d146354b464f9070ab9ac1
Young Seo Kim <youngseokim7@gmail.com>
2025-08-04 16:26:53 +0900

    compltete fix-score-type

--- src/scripts/fix-score-type.ts
+++ src/scripts/fix-score-type.ts
@@ -1,18 +1,22 @@
 import { mongoService } from "../sevices/MongoService";
 import type { Collection, Db } from "mongodb";
 
-const db: Db = mongoService.getDb();
-const collection: Collection = db.collection("tasks");
+(async () => {
+  const db: Db = mongoService.getDb();
+  const collection: Collection = db.collection("tasks");
 
-await collection.updateMany(
-  { score: { $type: "string" } }, // Only update documents where score is a string
-  [
-    {
-      $set: {
-        score: { $toInt: "$score" },
+  console.log("updating many:");
+  await collection.updateMany(
+    { score: { $type: "string" } }, // Only update documents where score is a string
+    [
+      {
+        $set: {
+          score: { $toInt: "$score" },
+        },
       },
-    },
-  ],
-);
+    ],
+  );
 
-mongoService.disconnect();
+  console.log("done updating, disconnecting client");
+  mongoService.disconnect();
+})();
